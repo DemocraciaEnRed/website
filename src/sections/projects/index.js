d746a1cadb5698d771df0e83bfe45ae3b2e3898f
Jose Fresco <josemariafresco@gmail.com>
2015-10-07 16:55:02 -0300

    Refactor

--- src/sections/projects/index.js
+++ src/sections/projects/index.js
@@ -1,12 +1,17 @@
 var dom = require('dominus')
 
+function reset () {
+  dom('.project-card').removeClass('expanded')
+}
+
 var cards = dom('.project-card .expand')
 cards.on('click', function () {
-  dom('.project-card').removeClass('expanded')
+  reset()
   dom(this).parents('.project-card').addClass('expanded')
 })
 
 var closeButtons = dom('.description-overlay button.close')
 closeButtons.on('click', function () {
-  dom(this).parents('.project-card').removeClass('expanded')
+  reset()
 })
+
