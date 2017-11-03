2d0be71c533bd545746ce7f3f728f22ef3f8bed5
trixtar <rita@democracyos.io>
2017-11-03 17:29:23 -0300

    adding links

--- containers/Navbar.js
+++ containers/Navbar.js
@@ -1,35 +1,42 @@
 
 let activeNavLink = null;
 
-function changeActiveNavLink (obj) {
-console.log(obj)
-  // if (!activeNavLink === null) {
-  //   obj.className='active'
-  // } else {
-  //   activeNavLink.className.replace(' active ','')
-  //   obj.className= obj.className + ' active '
-  // }
-
-  // activeNavLink = obj;
+function changeActiveNavLink (e) {
+  if (activeNavLink) {
+    activeNavLink.className.replace(' active' , '')
+    e.target.className = e.target.className + ' active'
+  } else {
+    e.target.className = e.target.className + ' active'
+  }
+  activeNavLink = e.target;
 }
 
+const links = [
+{
+  href: "/",
+  text: "Selected"
+},
+{
+  href: "#header",
+  text: "Item"
+},
+{
+  href: "#about",
+  text: "Item"
+}
+]
 
 const Navbar = () => (
   <nav role='banner'>
     <a className='logo' href='/'></a>
     <ul role='navigation'>
-      <li>
-        <a className='nav-link' onClick={changeActiveNavLink(this)}>Selected</a>
-      </li>
-      <li>
-        <a className='nav-link' onClick={changeActiveNavLink(this)}>Item</a>
-      </li>
-      <li>
-        <a className='nav-link' href='#'>Item</a>
-      </li>
-      <li>
-        <a className='nav-link' href='#'>Item</a>
-      </li>
+      {links.map((link, i) => {
+        return (
+          <li key={i}>
+            <a className='nav-link' onClick={changeActiveNavLink}>{link.text}</a>
+          </li>
+        )
+      })}
     </ul>
     <style jsx>{`
       nav {
@@ -38,7 +45,7 @@ const Navbar = () => (
         display: flex;
         justify-content: space-between;
         align-items: center;
-        background-color: #c5c5c5;
+        background-color: var(--dark-accent);
         box-shadow: inset 0 -1px 0 0 #555555;
 
       }
@@ -66,16 +73,18 @@ const Navbar = () => (
 
       .nav-link {
         padding: 15px 0;
-        font-family: 'Dosis-Regular';
-        font-size: 24px;
-        color: #6a6a6a;
+        font-family: var(--regular);
+        font-size: 2.4rem;
+        color: var(--white);
         text-transform: uppercase;
         text-decoration: none;
         letter-spacing: 0.7px;
+        cursor: pointer;
       }
 
       .active {
         font-weight: bold;
+        color: var(---black);
       }
 
     `}</style>
