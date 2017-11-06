a2ebbaca9ac1143f10d61d1752dc3d88eb4bd0d3
trixtar <rita@democracyos.io>
2017-11-06 15:12:54 -0300

    media queries

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,6 @@ export default () => (
       <link rel="stylesheet" type="text/css" href="/static/global.css" />
     </Head>
     <Layout>
-      <section>
-        <h1>Helloooooooo wooorld</h1>
-        <button className='btn'><span className='action-text'>Haz click!</span></button>
-      </section>
     </Layout>
   </div>
   )
