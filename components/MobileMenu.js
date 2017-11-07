eb0ddb2fa90e96b0037b204df28b1d5806e2f1c7
trixtar <rita@democracyos.io>
2017-11-07 15:24:26 -0300

    style fixes

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -28,7 +28,7 @@ const MobileMenu = (props) => (
       {secondaryLinks.map((link, i) => {
         return (
           <li key={i}>
-            <a className='nav-link'>{link.text}</a>
+            <a className='mobile-link'>{link.text}</a>
           </li>
         )
       })}
@@ -44,6 +44,7 @@ const MobileMenu = (props) => (
         flex-direction: column;
         width: 100vw;
         background-color: var(--white);
+        overflow-y: auto;
       }
 
       header {
@@ -52,7 +53,7 @@ const MobileMenu = (props) => (
       }
 
       .logo {
-        margin: 6px 8px 16px 0;
+        margin: 6px 8px 8px 0;
         width: 135px;
         height: 35px;
         background-image: url('/static/assets/logo-mobile.svg');
@@ -69,20 +70,30 @@ const MobileMenu = (props) => (
         background-image: url('/static/assets/close-mobile.svg');
         background-size: contain;
         background-repeat: no-repeat;
-
-      ul.primary-links {
-        display: block;
-        margin: 16px 0 0 72px;
-        padding-top: 32px;
-        border-top: solid 2px var(--dark-accent);
-        min-height: 50px;
       }
 
-      .nav-link {
+      .primary-links, .secondary-links {
+        display: inline-block;
+        margin: 24px 0 0 72px;
+        width: fit-content;
+        padding-top: 24px;
+        border-top: solid 2px var(--dark-accent);
+      }
+
+      .primary-links li, .secondary-links li {
+        padding: 8px 0;
+      }
+
+      .mobile-link {
         margin: 16px 0;
+        color: var(--dark-gray);
         font-family: var(--regular);
         font-size: 2.4rem;
         text-transform: uppercase;
+      }
+
+      .mobile-link:hover{
+        color: var(--dark-accent);
       }
 
       @media screen and (min-width: 860px) {
