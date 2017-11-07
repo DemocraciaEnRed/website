55f813db08bcc26a958b091fe73089828d3b8af6
florlafuente <lafuente.florencia@gmail.com>
2017-11-07 13:27:35 -0300

    Finishes header styles

--- components/Submit.js
+++ components/Submit.js
@@ -4,12 +4,17 @@ export default class Submit extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      checked: false
+      checked: false,
+      hasClicked: false
     }
   }
 
   checkingButton = () => {
-    this.setState({checked: true})
+    this.isChecked = true
+    this.setState({
+      checked: true,
+      hasClicked: true
+    })
     setTimeout(() => this.setState({
       checked: false,
     }), 2000)
@@ -20,9 +25,13 @@ export default class Submit extends React.Component {
       <div className='submit'>
         <input type='email' />
         <button onClick={this.checkingButton}>
-          <span className='submit-text'>
-      	 	 {this.state.checked ? 'Cambie' : 'Submit'} 
-          </span>
+          {this.state.checked ?
+            <div className='checkbox' />
+           :
+            <span className={`submit-text ${this.state.hasClicked ? 'submit-text-checked' : ''}`}>
+      	 	   Submit
+            </span>
+          }
         </button>
         <style jsx>{`
           .submit {
@@ -44,10 +53,32 @@ export default class Submit extends React.Component {
     		    border: none;
     		    border-radius: 0 10rem 10rem 0;
   			    color: var(--white);
+            display: flex;
+            justify-content: center;
     		    height: 100%;
     		    width: 25%;
     	    }
-          @media (max-width: 500px) {
+          .submit-text-checked {
+            -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
+            -moz-animation: fadein 2s; /* Firefox < 16 */
+            -ms-animation: fadein 2s; /* Internet Explorer */
+            -o-animation: fadein 2s; /* Opera < 12.1 */
+            animation: fadein 0.5s;
+          }
+
+          @keyframes fadein {
+            from { opacity: 0; }
+            to   { opacity: 1; }
+          }
+          .submit button .checkbox {
+            background-image: url('/static/assets/checked.svg');
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center;
+            width: 40px;
+            height: 40px;
+          }
+          @media (max-width: 1024px) {
             .submit {
               max-width: 100%;
               height: 34px;
