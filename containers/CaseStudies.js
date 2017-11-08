28b62a4cd7c27549f45cc7578d955445926d692d
florlafuente <lafuente.florencia@gmail.com>
2017-11-08 14:17:25 -0300

    Changes to stateful

--- containers/CaseStudies.js
+++ containers/CaseStudies.js
@@ -1,7 +1,8 @@
+import React, { Component } from 'react'
 import dynamic from 'next/dynamic'
 import CaseStudiesCard from '../components/CaseStudiesCard'
 
-const DynamicComponent = dynamic(import Flickity from ('flickity'))
+const DynamicComponent = dynamic(import ('flickity'))
 
 const example = [
   {title: 'Lorem ipsum dolor sit amet', subtitle: 'Lorem ipsum dolor sit amet'},
@@ -10,54 +11,58 @@ const example = [
   {title: 'Lorem ipsum dolor sit amet', subtitle: 'Lorem ipsum dolor sit amet'}
 ]
 
-const CaseStudies = () => (
-  <section className='case-studies-section'>
-    <h2 className='section-title'>
-      Case Studies
-    </h2>
-    <div className='case-studies-container'>
-      {example.map((card, i) => {
-        return <CaseStudiesCard key={i} title={card.title} subtitle={card.subtitle} />
-      })}
-    </div>
-    <button className='btn'>
-      <span className='action-text'>
-        Ver más
-      </span>
-    </button>
-    <style jsx>{`
-      .case-studies-section {
-        display: flex;
-        flex-direction: column;
-      }
-      .case-studies-section .case-studies-container{
-        display: flex;
-        justify-content: space-between;
-      }
-      .case-studies-section .btn {
-        align-self: center;
-      }
-      .case-studies-section .btn .action-text {
-        color: var(--white);
-      }
-      @media (max-width: 500px) {
-        .case-studies-section .section-title {
-          font-size: 3.0rem;
-        }
-        .case-studies-section .case-studies-container {
+class CaseStudies extends Component  {
+  render () {
+    return (
+      <section className='case-studies-section'>
+        <h2 className='section-title'>
+          Case Studies
+        </h2>
+        <div className='case-studies-container'>
+          {example.map((card, i) => {
+            return <CaseStudiesCard key={i} title={card.title} subtitle={card.subtitle} />
+          })}
+        </div>
+        <button className='btn'>
+          <span className='action-text'>
+            Ver más
+          </span>
+        </button>
+      <style jsx>{`
+        .case-studies-section {
+          display: flex;
           flex-direction: column;
-          align-items: center;
+        }
+        .case-studies-section .case-studies-container{
+          display: flex;
+          justify-content: space-between;
         }
         .case-studies-section .btn {
-          height: 36.7px;
-          width: 200px;
+          align-self: center;
         }
         .case-studies-section .btn .action-text {
-          font-size: 2.1rem;
+          color: var(--white);
         }
-      }
+        @media (max-width: 500px) {
+          .case-studies-section .section-title {
+            font-size: 3.0rem;
+          }
+          .case-studies-section .case-studies-container {
+            flex-direction: column;
+            align-items: center;
+          }
+          .case-studies-section .btn {
+            height: 36.7px;
+            width: 200px;
+          }
+          .case-studies-section .btn .action-text {
+            font-size: 2.1rem;
+          }
+        }
     `}</style>
   </section>
-)
+  )
+}
+}
 
 export default CaseStudies
