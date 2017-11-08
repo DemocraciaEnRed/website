cd15a3ce719f737f35e9edd4a5b0be5b3347ae43
florlafuente <lafuente.florencia@gmail.com>
2017-11-08 16:49:57 -0300

    Adds require flickity

--- containers/CaseStudies.js
+++ containers/CaseStudies.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import dynamic from 'next/dynamic'
 import CaseStudiesCard from '../components/CaseStudiesCard'
-
-const DynamicComponent = dynamic(import ('flickity'))
+let Flickity;
 
 const example = [
   {title: 'Lorem ipsum dolor sit amet', subtitle: 'Lorem ipsum dolor sit amet'},
@@ -12,16 +11,49 @@ const example = [
 ]
 
 class CaseStudies extends Component  {
+  constructor (props) {
+    super(props)
+    this.flkty = null
+    this.state = {
+      mobile: false
+    }
+  }
+
+  componentDidMount () {
+    Flickity = require('flickity')
+    if (window.innerWidth < 1024) {
+      this.setState({
+        mobile: true
+      })
+    }
+  }
+
+  componentDidUpdate(){
+    if (this.flkty) this.flkty.destroy()
+    const options = {
+      animate: true,
+      pageDots: false,
+      autoplay: true,
+      cellAlign: 'center'
+      
+    }
+    this.flkty = new Flickity(this.refs.carousel, options)
+  }
+
+  componentWillUnmount () {
+    this.flkty.destroy()
+  }
+
   render () {
     return (
       <section className='case-studies-section'>
         <h2 className='section-title'>
           Case Studies
         </h2>
-        <div className='case-studies-container'>
-          {example.map((card, i) => {
-            return <CaseStudiesCard key={i} title={card.title} subtitle={card.subtitle} />
-          })}
+        <div className='case-studies-container carousel' ref='carousel'>
+          {example.map((card, i) => (
+            <CaseStudiesCard key={i} title={card.title} subtitle={card.subtitle} />
+          ))}
         </div>
         <button className='btn'>
           <span className='action-text'>
@@ -43,13 +75,12 @@ class CaseStudies extends Component  {
         .case-studies-section .btn .action-text {
           color: var(--white);
         }
-        @media (max-width: 500px) {
+        @media (max-width: 1024px) {
+          .case-studies-section .case-studies-container {
+
+          }
           .case-studies-section .section-title {
             font-size: 3.0rem;
-          }
-          .case-studies-section .case-studies-container {
-            flex-direction: column;
-            align-items: center;
           }
           .case-studies-section .btn {
             height: 36.7px;
@@ -59,6 +90,7 @@ class CaseStudies extends Component  {
             font-size: 2.1rem;
           }
         }
+        @import '../node_modules/flickity/css/flickity.css'
     `}</style>
   </section>
   )
