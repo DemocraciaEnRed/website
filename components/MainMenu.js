e9d35af1d3fc9970a291a639ab9d892b02a5170c
trixtar <rita@democracyos.io>
2017-11-08 15:16:03 -0300

    major navbar change

--- components/MainMenu.js
+++ components/MainMenu.js
@@ -10,17 +10,17 @@ const secondaryLinks = [
 ]
 
 
-const MobileMenu = (props) => (
-  <nav className='mobile-menu' role='navigation'>
+const MainMenu = (props) => (
+  <nav className='main-menu' role='navigation'>
     <header>
       <a className='logo' href='/'></a>
-      <a className='close-menu' onClick={props.handleMobileMenu}></a>
+      <a className='close-menu' onClick={props.handleMainMenu}></a>
     </header>
     <ul className='primary-links'>
       {props.links.map((link, i) => {
         return (
           <li key={i}>
-            <a onClick={props.handleActiveLink(link.text)} className={`mobile-link ${props.activeLink === link.text ? ' active' : ''}`}>{link.text}</a>
+            <a onClick={props.handleActiveLink(link.text)} className={`menu-link ${props.activeLink === link.text ? ' active' : ''}`}>{link.text}</a>
           </li>
         )
       })}
@@ -29,13 +29,13 @@ const MobileMenu = (props) => (
       {secondaryLinks.map((link, i) => {
         return (
           <li key={i}>
-            <a onClick={props.handleActiveLink(link.text)} className={`mobile-link ${props.activeLink === link.text ? ' active' : ''}`}>{link.text}</a>
+            <a onClick={props.handleActiveLink(link.text)} className={`menu-link ${props.activeLink === link.text ? ' active' : ''}`}>{link.text}</a>
           </li>
         )
       })}
     </ul>
     <style>{`
-      .mobile-menu {
+      .main-menu {
         position: absolute;
         top: 0;
         left: 0;
@@ -70,9 +70,10 @@ const MobileMenu = (props) => (
         width: 20px;
         height: 20px;
         flex: none;
-        background-image: url('/static/assets/close-mobile.svg');
+        background-image: url('/static/assets/close-menu.svg');
         background-size: contain;
         background-repeat: no-repeat;
+        cursor: pointer;
       }
 
       .primary-links, .secondary-links {
@@ -87,7 +88,7 @@ const MobileMenu = (props) => (
         padding: 8px 0;
       }
 
-      .mobile-link {
+      .menu-link {
         margin: 16px 0;
         color: var(--dark-gray);
         font-family: var(--regular);
@@ -95,17 +96,11 @@ const MobileMenu = (props) => (
         text-transform: uppercase;
       }
 
-      .active, .mobile-link:hover {
+      .active, .menu-link:hover {
         color: var(--dark-accent);
-      }
-
-      @media screen and (min-width: 860px) {
-        .mobile-menu {
-          display: none;
-        }
       }
     `}</style>
   </nav>
 )
 
-export default MobileMenu
+export default MainMenu
--- containers/Navbar.js
+++ containers/Navbar.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
-import MobileMenu from '../components/MobileMenu'
+import MainMenu from '../components/MainMenu'
 
 class Navbar extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      mobile: false,
+      menu: false,
       active: null
     }
-
   }
 
 handleActiveLink = (link) => (e) => {
@@ -18,10 +17,10 @@ handleActiveLink = (link) => (e) => {
   })
 }
 
-handleMobileMenu = (e) => {
+handleMainMenu = (e) => {
   e.preventDefault()
   this.setState({
-    mobile: !this.state.mobile
+    menu: !this.state.menu
   })
 }
 
@@ -56,20 +55,11 @@ render () {
   return (
     <nav role='banner'>
       <a className='logo' href='/'></a>
-      <ul className='nav-wide' role='navigation'>
-        {links.map((link, i) => {
-          return (
-            <li key={i}>
-              <a className='nav-link' className={`nav-link ${this.state.active === link.text ? ' active' : ''}`} onClick={this.handleActiveLink(link.text)}>{link.text}</a>
-            </li>
-          )
-        })}
-      </ul>
-      <a className='mobile-button' role='navigation' onClick={this.handleMobileMenu}></a>
-      {this.state.mobile &&
-        <MobileMenu
+      <a className='menu-button' role='navigation' onClick={this.handleMainMenu}></a>
+      {this.state.menu &&
+        <MainMenu
         links={links}
-        handleMobileMenu={this.handleMobileMenu}
+        handleMainMenu={this.handleMainMenu}
         handleActiveLink={this.handleActiveLink}
         activeLink={this.state.active} />
       }
@@ -77,8 +67,8 @@ render () {
         nav {
           position: fixed;
           width: 100vw;
-          height: 100px;
-          padding: 0 72px;
+          height: 50px;
+          padding: 0 42px;
           display: flex;
           justify-content: space-between;
           align-items: center;
@@ -87,88 +77,29 @@ render () {
         }
         
         .logo {
-          width: 211px;
-          height: 58px;
-          background-image: url('/static/assets/logo.svg');
+          width: 106px;
+          height: 30px;
+          background-image: url('/static/assets/logo-der.svg');
           background-size: contain;
           background-repeat: no-repeat;
           flex: none;
-          margin-right: 20px;
         }
 
-        ul.nav-wide {
-          display: inline-flex;
-          flex-wrap: wrap;
-          justify-content: flex-end;
-          text-align: center;
-        }
-
-        ul li {
+        .menu-button {
           display: inline-block;
-        }
-
-        li + li {
-          margin-left: 45px;
-        }
-
-        .nav-link {
-          padding: 15px 0;
-          font-family: var(--regular);
-          font-size: 2.4rem;
-          color: var(--white);
-          text-transform: uppercase;
-          letter-spacing: 0.7px;
+          width: 18px;
+          height: 13.5px;
+          flex: none;
+          background-image: url('/static/assets/open-menu.svg');
+          background-size: contain;
+          background-repeat: no-repeat;
           cursor: pointer;
         }
 
-        .active {
-          font-weight: bold;
-          color: var(---black);
-        }
-
-        .mobile-button {
-          display: none;
-        }
-
-        MobileMenu {
-          display: none;
-        }
-
-        MobileMenu .visible {
-          display: flex;
-        }
-
-        @media screen and (max-width: 1024px) {
+        @media screen and (max-width: 860px) {
           nav {
             padding: 0 24px;
           }
-        }
-
-
-        @media screen and (max-width: 860px) {
-          nav {
-            height: 64px;
-          }
-
-          .logo {
-            width: 125px;
-            height: 35px;
-          }
-
-          ul.nav-wide {
-            display: none;
-          }
-
-          .mobile-button {
-            display: inline-block;
-            width: 18px;
-            height: 13.5px;
-            flex: none;
-            background-image: url('/static/assets/nav-mobile.svg');
-            background-size: contain;
-            background-repeat: no-repeat;
-          }
-
         }
 
       `}</style>
