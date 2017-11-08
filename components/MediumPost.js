8247639dac70a2c45c4ecc2e93e0932d8ce3dc31
trixtar <rita@democracyos.io>
2017-11-08 17:18:06 -0300

    basic styling

--- components/MediumPost.js
+++ components/MediumPost.js
@@ -1,13 +1,53 @@
 const MediumPost = (props) => (
   <article className='medium-post'>
-    <p className='text-snippet'>{props.snippet}</p>
+    <div className='post-snippet'>
+      <p className='regular-text snippet-text'>{props.snippet}</p>
+    </div>
     <div className='post-data'>
       <span className='timestamp'>{props.timestamp}</span>
-      <i className='heart'></i>
-      <span className='likes'>{props.likes}</span>
+      <span className='likes'><img className='heart' src='/static/assets/heart.svg' />{props.likes}</span>
     </div>
   <style jsx>{`
+    .medium-post {
+      margin-right: 72px;
+      width: 371px;
+      height: 305px;
+      padding: 38px 20px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      background-color: rgba(0,0,0,0.82);
+    }
 
+    .post-snippet {
+      display: flex;
+      align-items: center;
+      flex-grow: 1;
+    }
+
+    .snippet-text {
+      font-size: 2.4rem;
+    }
+
+    .post-data {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      flex: none;
+    }
+
+    .post-data span {
+      font-family: var(--regular);
+      font-size: 2rem;
+      text-transform: uppercase;
+      color: var(--white);
+    }
+
+    .likes .heart {
+      width: 21px;
+      height: 20px;
+      margin-right: 15px;
+    }
   `}</style>
   </article>
 )
--- containers/Publications.js
+++ containers/Publications.js
@@ -7,16 +7,21 @@ let likes = 123
 const Publications = () => (
   <section className='publications-section'>
     <h2 className='section-title'>Publicaciones</h2>
-    <MediumPost
-      snippet={snippet}
-      timestamp={timestamp}
-      likes={likes} />
-    <MediumPost
-      snippet={snippet}
-      timestamp={timestamp}
-      likes={likes} />
+    <div className='posts-container'>
+      <MediumPost
+        snippet={snippet}
+        timestamp={timestamp}
+        likes={likes} />
+      <MediumPost
+        snippet={snippet}
+        timestamp={timestamp}
+        likes={likes} />
+    </div>
   <style jsx>{`
-    
+    .posts-container {
+      margin: 43px 0;
+      display: flex;
+    }
   `}</style>
   </section>
 )
