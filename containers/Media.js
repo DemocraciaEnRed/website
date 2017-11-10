79b8b663100756a3ec7429d67f4aed440717f6bd
florlafuente <lafuente.florencia@gmail.com>
2017-11-10 12:58:33 -0300

    Adds media container and media assets

---
+++ containers/Media.js
@@ -0,0 +1,9 @@
+const Media = () => (
+  <section className='media-section'>
+    <h2 className='section-title'>Media</h2>
+    <div className='media-container'>
+    </div>
+  </section>
+)
+
+export default Media
