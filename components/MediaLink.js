768709ad95a8167d9cb14cc3314a63e7503b339f
florlafuente <lafuente.florencia@gmail.com>
2017-11-10 15:39:34 -0300

    Finishes mobile styles

--- components/MediaLink.js
+++ components/MediaLink.js
@@ -1,9 +1,48 @@
-const MediaLink = ( {url, logo}) => (
-  <a href ={url} target='_blank' rel='external'>
-    <img src={logo} className='media-link-image'/>
+const MediaLink = ( {url, logo, className}) => (
+  <a href ={url} target='_blank' rel='external' className={className}>
+    <img src={logo} className='media-link-image' />
     <style jsx>{`
       .media-link-image {
-        height: 62px;
+        height: 60px;
+      }
+      @media screen and (max-width: 1024px) {
+        .media-link-image {
+          height: 40px;
+        }
+      }
+      @media screen and (max-width: 768px) {
+        .media-link-image {
+          height: 30px;
+        }
+      }
+      @media screen and (max-width: 600px) {
+        .wired {
+          order: 1;
+        }
+        .ted {
+          order: 2;
+        }
+        .fast {
+          order: 3;
+        }
+        .elpais {
+          order: 4;
+        }
+        .lanacion {
+          order: 5;
+        }
+        .lpo {
+          order: 6;
+        }
+        .bbc {
+          order: 7;
+          margin: auto;
+        }
+      }
+      @media screen and (max-width: 375px) {
+        .bbc {
+          margin: 0;
+        }
       }
     `}</style>
   </a>
