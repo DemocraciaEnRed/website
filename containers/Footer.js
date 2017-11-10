0d9ebe349961d6ac0cba15905c5d6c66da2cf538
florlafuente <lafuente.florencia@gmail.com>
2017-11-10 17:40:02 -0300

    basic footer structure

--- containers/Footer.js
+++ containers/Footer.js
@@ -7,6 +7,16 @@ const Footer = () => (
     <Link href=''>
       <a>Términos y Condiciones</a>
     </Link>
+    <style jsx>{`
+      footer {
+        align-items: flex-end;
+        background-color: #383737;
+        display: flex;
+        flex-wrap: wrap;
+        padding: 455px 115px 33px;
+      }
+
+    `}</style>
   </footer>
 )
 
