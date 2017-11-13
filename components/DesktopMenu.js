62133975f5176f34daa038453b5baee2cea1af93
florlafuente <lafuente.florencia@gmail.com>
2017-11-13 18:08:35 -0300

    Desktop menu styles

--- components/DesktopMenu.js
+++ components/DesktopMenu.js
@@ -1,4 +1,4 @@
-const links = [
+const primaryLinks = [
   {
     href: '/',
     title: 'About us'
@@ -30,7 +30,10 @@ const links = [
   {
     href: '/',
     title: 'Who we are'
-  },
+  }
+]
+
+const secondaryLinks = [
   {
     href: '/',
     title: 'Contact us'
@@ -44,8 +47,16 @@ const links = [
 
 const DesktopMenu = (props) => (
   <nav className='main-menu' role='navigation'>
-    <ul className='links-container'>
-      {links.map((link, i)=> 
+    <a className='close-menu' onClick={props.handleMainMenu}></a>
+    <ul className='primary-links-container'>
+      {primaryLinks.map((link, i)=> 
+        <li key={i}>
+          <a onClick={props.handleActiveLink(link.title)} className={`desktop-menu-link ${props.activeLink === link.title ? ' active' : ''}`}>{link.title}</a>
+        </li>
+      )}
+    </ul>
+    <ul className='secondary-links-container'>
+      {secondaryLinks.map((link, i)=> 
         <li key={i}>
           <a onClick={props.handleActiveLink(link.title)} className={`desktop-menu-link ${props.activeLink === link.title ? ' active' : ''}`}>{link.title}</a>
         </li>
@@ -53,17 +64,58 @@ const DesktopMenu = (props) => (
     </ul>
     <style jsx>{`
       .main-menu {
+        align-content: space-between;
         background-color: var(--white);
         box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.57);
+        display: flex;
+        flex-wrap: wrap;
         height: 451px;
-        padding: 113px 202px 126px 90px;
+        left: 0;
+        right: 0;
+        margin: 61px auto;
+        padding: 18px 18px 126px 90px;
+        position: absolute;
+        top: 0;
         width: 1420px;
+        z-index: 99;
+      }
+      .close-menu {
+        position: absolute;
+        right: 0;
+        align-self: flex-start;
+        display: block;
+        width: 18px;
+        height: 18px;
+        background-image: url('/static/assets/close-menu.svg');
+        background-size: contain;
+        background-repeat: no-repeat;
+        cursor: pointer;
+        margin-bottom: 62px;
+      }
+      .primary-links-container {
+        align-items: space-between;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        height: 261px;
+        justify-content: space-between;
+        width: 680px;
+      }
+      .secondary-links-container {
+        align-items: space-between;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+        width: 168px;
       }
       .desktop-menu-link {
+        display: block;
         color: var(--black);
         font-size: 3.2rem;
         letter-spacing: 1.1px;
         text-transform: uppercase;
+        margin-bottom: 16px;
       }
     `}</style>
   </nav>
