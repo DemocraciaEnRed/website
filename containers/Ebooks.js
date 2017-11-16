0c8520fdc8a65d7e9d54c0f2bd7768c43988fd42
florlafuente <lafuente.florencia@gmail.com>
2017-11-16 12:43:33 -0300

    Changes in ebooks section

--- containers/Ebooks.js
+++ containers/Ebooks.js
@@ -1,4 +1,6 @@
+import React, { Component } from 'react'
 import EbookCard from '../components/EbookCard'
+let Flickity;
 
 const dataEbooks = [
   {
@@ -15,36 +17,46 @@ const dataEbooks = [
   }
 ]
 
-const Ebooks = () => (
-  <section className='ebooks-section'>
-    <h2 className='section-title'>E-books</h2>
-    <div className='ebooks-container'>
-      {dataEbooks.map((item,i) => 
-        <EbookCard key={i} img={item.img} title={item.title} description={item.description} link={item.link} />
-      )}
-    </div>
-    <style jsx>{`
-      .ebooks-container {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-        margin: 59px 0 118px;
-      }
-      @media screen and (max-width: 1024px) {
-        .ebooks-section {
-          padding: 25px 25px 144px 25px ;
-        }
-        .ebooks-container {
-          margin: 84px 0 0;
-        }
-      }
-      @media screen and (max-width: 500px) {
-        .ebook-container {
-          justify-content: center;
-        }
-      }
-    `}</style>
-  </section>
-)
+class Ebooks extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      mobile: false
+    }
+  }
+  render () {
+    return (
+      <section className='ebooks-section'>
+        <h2 className='section-title'>E-books</h2>
+        <div className='ebooks-container'>
+          {dataEbooks.map((item,i) => 
+            <EbookCard key={i} img={item.img} title={item.title} description={item.description} link={item.link} />
+          )}
+        </div>
+        <style jsx>{`
+          .ebooks-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+            margin: 59px 0 118px;
+          }
+          @media screen and (max-width: 1024px) {
+            .ebooks-section {
+              padding: 24px 24px 111px 24px ;
+            }
+            .ebooks-container {
+              margin: 17px 0 0;
+            }
+          }
+          @media screen and (max-width: 500px) {
+            .ebook-container {
+              justify-content: center;
+            }
+          }
+        `}</style>
+      </section>
+    )
+  }
+}
 
 export default Ebooks
