c7b4235412d98ebfbc1f62f216faf7a34d06019f
florlafuente <lafuente.florencia@gmail.com>
2017-11-24 12:57:02 -0300

    Adds border to CivicStack

--- sections/about/components/CivicStack.js
+++ sections/about/components/CivicStack.js
@@ -8,13 +8,30 @@ const CivicStack = () => (
     </button>
     <style jsx>{`
       .civic-stack-container {
-        border-bottom-style: solid;
-        border-top-style: solid;
         display: flex;
         flex-wrap: wrap;
         justify-content: center;
         padding-bottom: 100px;
         padding-top: 100px;
+        position: relative;
+      } 
+      .civic-stack-container:before {
+        border-bottom:1px solid #979797;
+        content : '';
+        height: 1px;
+        left: 10%;
+        position: absolute;
+        top: 0;
+        width: 80%;
+      } 
+      .civic-stack-container:after{
+        content : '';
+        position: absolute;
+        left    : 10%;
+        bottom  : 0;
+        height  : 1px;
+        width   : 80%;
+        border-bottom:1px solid #979797;
       }
       .civic-stack-text {
         font-size: 2rem;
