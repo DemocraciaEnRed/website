0319468b113c882eef0cbb60c58369a7e8f53d1b
florlafuente <lafuente.florencia@gmail.com>
2017-12-04 16:22:28 -0300

    Fixes next.config.js bug

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     exportPathMap: function () {
         return {
             '/': { page: '/' },
-            '/about' : {  page: 'about' }
+            '/about' : {  page: '/about' }
         }
     }
 }
