2524e9c791fbaaf95758c0686476ff8b2ef09810
florlafuente <lafuente.florencia@gmail.com>
2017-12-07 10:56:29 -0300

    Finishes JobCard styles

--- sections/jobs/components/JobCard.js
+++ sections/jobs/components/JobCard.js
@@ -1,19 +1,50 @@
 const JobCard = () => (
-  <div>
-    <span>Desarrollo</span>
-    <h5>Back-end dev</h5>
-    <ul>
+  <div className='job-card'>
+    <span className='job-card-category'>Desarrollo</span>
+    <h5 className='job-card-title'>Back-end dev</h5>
+    <ul className='job-card-list'>
       <li>Semi-senior</li>
       <li>Full-time</li>
       <li>Oficina/WFH</li>
     </ul>
-    <button className='btn'>
-      <span className='action-text'>
+    <button className='btn job-card-btn'>
+      <span className='action-text job-card-action-text'>
         Aplicar
       </span>
     </button>
     <style jsx>{`
-
+      .job-card {
+        align-items: flex-start;
+        background-color: #f0f0f0;
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: column;
+        height: 268px;
+        justify-content: space-between;
+        padding: 16px;
+        width: 313px;
+      }
+      .job-card-category {
+        align-self: flex-end;
+        color: #565656;
+        font-size: 2.0rem;
+      }
+      .job-card-title {
+        color: var(--dark-accent);
+        font-size: 3.6rem;
+      }
+      .job-card-list li{
+        color: var(--gray);
+        font-size: 2.4rem;
+      }
+      .job-card-btn {
+        align-self: flex-end;
+        height: 31px;
+        width: 130px;
+      }
+      .job-card-action-text {
+        font-size: 1.8rem;
+      }
     `}</style>
   </div>
 )
