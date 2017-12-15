fa99ff754db4b4cd484ed46291784df8c6f004f6
florlafuente <lafuente.florencia@gmail.com>
2017-12-15 12:29:03 -0300

    Finishes footer layout

--- containers/Footer.js
+++ containers/Footer.js
@@ -8,67 +8,82 @@ const secondaryLinks = [0, 1]
 
 const Footer = () => (
   <footer>
-    <MediaQuery minDeviceWidth={1025} >
-      <div>
-        <p>{t('footer.copy')}</p>
-      </div>
-      <nav className='footer-navbar' role='navigation'>
-        <ul className='primary-links-container'>
-          {primaryLinks.map((i)=> {
-            return (
-              <li key={i}>
-                <a>  
-                  {t(`nav.primaryLinks.${i}.title`)}
-                </a>
-              </li>
-            )
-          })}
-        </ul>
-        <ul className='secondary-links-container'>
-        {secondaryLinks.map((i)=> {
-          return (
-            <Link href={t(`nav.secondaryLinks.${i}.href`)} key={i} >
-              <li>
-                <a>  
-                  {t(`nav.secondaryLinks.${i}.title`)}
-                </a>
-              </li>
-            </Link>
-          )
-        })}
-    </ul>
-      </nav>
-      <div className='media-bar-container'>
-        <SocialBar alignSelf={'flex-start'} iconsSize={'47px'} width={'473px'} mobileWidth={'283px'} iconHover={'#bebebe'} iconMobileHover={'#bebebe'} />
-        <div className='end-text'>
-          <span>the end</span>
-        </div>
-      </div>
+    <span className='footer-copy'>{t('footer.copy')}</span>
+    <nav className='footer-navbar' role='navigation'>
+      <ul className='primary-links-container'>
+        {primaryLinks.map((i)=> (
+          <li key={i}>
+            <a className='footer-navbar-link'>  
+              {t(`nav.primaryLinks.${i}.title`)}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <ul className='secondary-links-container'>
+        {secondaryLinks.map((i)=> (
+          <Link href={t(`nav.secondaryLinks.${i}.href`)} key={i} >
+            <li>
+              <a className='footer-navbar-link'>  
+                {t(`nav.secondaryLinks.${i}.title`)}
+              </a>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </nav>
+    <MediaQuery minDeviceWidth={1025}>
+      <SocialBar alignSelf={'flex-start'} iconsSize={'47px'} width={'331px'} mobileWidth={'283px'} iconHover={'#bebebe'} iconMobileHover={'#bebebe'} />
     </MediaQuery>
     <MediaQuery maxDeviceWidth={1024} >
-      <div className='media-bar-container'>
-        <SocialBar alignSelf={'flex-start'} iconsSize={'28px'}  width={'473px'} mobileWidth={'283px'} justifyContent={'space-between'} iconHover={'#bebebe'} iconMobileHover={'#bebebe'} />
-        <div className='end-text'>
-          <span>the end</span>
-        </div>
-      </div>
+      <SocialBar alignSelf={'flex-end'} iconsSize={'28px'}  width={'331px'} mobileWidth={'283px'} justifyContent={'space-between'} iconHover={'#bebebe'} iconMobileHover={'#bebebe'} />
     </MediaQuery>
+    <div className='end-text'>
+      <span>the end</span>
+    </div>
     <style jsx>{`
       footer {
-        align-items: flex-start;
+        align-items: center;
         background-color: #383737;
         display: flex;
         flex-direction: column;
         height: 596px;
-        justify-content: flex-end;
-        padding: 0 115px 33px;
+        justify-content: space-between;
+        padding: 71px 237px 33px;
         position: relative;
         overflow-y: hidden;
       }
-      .media-bar-container {
-        align-items: center;
+      .footer-copy {
+        color: var(--white);
+        font-size: 2.4rem;
+        letter-spacing: 0.08rem;
+      }
+      .footer-navbar {
         display: flex;
-        flex-direction: column;
+        flex-wrap: wrap;
+        height: 202px;
+        justify-content: space-between;
+        width: 776px;
+      }
+      .primary-links-container {
+        display: flex;
+        flex-wrap: wrap;
+        height: 202px;
+        width: 482px;
+      }
+      .primary-links-container li {
+        width: 50%;
+      }
+      .secondary-links-container {
+        width: 126px;
+      }
+      .secondary-links-container li{
+        margin-bottom: 18px;
+      }
+      .footer-navbar-link {
+        color: var(--white);
+        font-size: 2.4rem;
+        letter-spacing: 0.08rem;
+        text-transform: uppercase;
       }
       .end-text {
         position: absolute;
@@ -84,7 +99,14 @@ const Footer = () => (
         footer {
           align-items: center;
           height: 466px;
-          padding: 0 60px 60px;
+          justify-content: flex-end;
+          padding: 60px;
+        }
+        .footer-copy {
+          display: none;
+        }
+        .footer-navbar {
+          display: none;
         }
         .end-text  {
           bottom: -20px;
@@ -92,13 +114,6 @@ const Footer = () => (
         }
         .end-text span {
           font-size: 6rem;
-        }
-      }
-      @media (max-width: 768px) {
-        .media-bar-container {
-          display: flex;
-          justify-content: center;
-          width: 100%;
         }
       }
     `}</style>
