5195f0ec01c421252b11318ddc44dff7144eb06a
luarar <laurarh.971@gmail.com>
2018-05-08 15:09:23 -0300

    fix styles of last profile cards and a typo

--- sections/index/components/ProfileCard.js
+++ sections/index/components/ProfileCard.js
@@ -106,7 +106,14 @@ const ProfileCard = ( {name, bio, handle, img} ) => (
         margin-right: 10px;
       }
     }
-    
+    @media screen and (min-width: 1200px) {
+      .profile-card:nth-child(13) {
+        margin-left: 25%
+      }
+      .profile-card:nth-child(14) {
+        margin-right: 25%
+      }
+    }
     @media screen and (min-width: 1024px) {
       .profile-card:hover .profile-card-container {
         heigth:260px;
