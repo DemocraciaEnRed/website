d6beab4e01fbde7752abeee106a9d95a05a8feb6
luarar <laurarh.971@gmail.com>
2018-05-17 14:39:20 -0300

    add analytics functionalities

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -1,9 +1,25 @@
 import Head from 'next/head'
 import Navbar from '../containers/Navbar'
+import React from 'react'
+import { initGA, logPageView } from '../utils/analytics'
 
-const Layout = (props) => (
-  <div>
-    <Head>
+
+
+
+export default class Layout extends React.Component {
+  
+  componentDidMount () {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    logPageView()
+  }
+  
+  render () {
+    return (
+      <div>
+        <Head>
       <title>Democracia en Red</title>
       <meta name='viewport' content='width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0' />
       <link rel="shortcut icon" type="favicon" href="/static/assets/icons/der-favicon.ico" />
@@ -11,11 +27,11 @@ const Layout = (props) => (
       <link rel="stylesheet" type="text/css" href="/static/global.css" />
     </Head>
     <Navbar 
-      changeLang={props.changeLang}
-      currentLang={props.currentLang}
+      changeLang={this.props.changeLang}
+      currentLang={this.props.currentLang}
     />
-    {props.children}
-  </div>
-)
-
-export default Layout
+    {this.props.children}
+      </div>
+    )
+  }
+}
