170f2fe588d3e8221c04c7dafa5873ad1384d582
florlafuente <lafuente.florencia@gmail.com>
2018-06-11 20:45:21 -0300

    Add next dynamic

--- sections/about/containers/Content.js
+++ sections/about/containers/Content.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import dynamic from 'next/dynamic'
 import DisplayVideo from '../components/DisplayVideo'
 const SliderContainer  = dynamic(import('./SliderContainer'), {
   ssr: false
