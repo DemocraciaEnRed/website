de744960fa6766eb29b2ec9fc7d896a2e308baf5
Your Name <inveniahadouken@gmail.com>
2019-04-03 19:09:21 -0300

    changed projects.js for transparency.js

---
+++ pages/transparency.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import { polyglot } from '../polyglot-modules/polyglot'
+import { t } from '../polyglot-modules/polyglot.js'
+import es from '../translations/es.json'
+import en from '../translations/en.json'
+import Layout from '../layouts/Layout'
+import Header from '../containers/Header'
+//import Transparency from '../sections/transparency/containers/AboutUs'
+import Content from '../sections/transparency/containers/Content'
+import Footer from '../containers/Footer'
+
+export default class extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      currentLang : 'es'
+    }
+    if (Object.values(polyglot.phrases).length === 0) {
+      polyglot.extend(es)
+      polyglot.locale(es.language)
+    }
+    
+  }
+
+  componentDidMount () {
+    const lang = localStorage.getItem('lang')
+    if (lang === null) {
+      localStorage.setItem('lang', 'es')
+    } else if (lang !== polyglot.currentLocale) {
+      this.changeLang('en')
+    }
+  }
+
+  changeLang = (lang) => {
+    const newLang = lang === 'es' ? es : en
+    polyglot.extend(newLang) 
+    polyglot.locale(newLang.language)
+    localStorage.setItem('lang', lang)
+    this.setState({currentLang: lang})
+  }
+  
+  render () {
+    return (
+      <div>
+        <Layout 
+          changeLang={this.changeLang}
+          currentLang={this.state.currentLang} >
+          <Header 
+            title={t('transparency.title')}
+            bg={'../static/assets/header/headerWeb.jpg'} />
+         {/*  <AboutUs />*/}
+          <Content icon={t('transparency.logo')}/>
+          <h2>Hola</h2>
+          <Footer />
+        </Layout>
+        <style jsx>{`
+          section {
+            padding-left: 100px;
+            padding-right: 100px;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
