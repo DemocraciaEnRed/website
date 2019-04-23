737d6424377eb5f5ce09327bb07836763230caa1
Inventrixx <inveniahadouken@gmail.com>
2019-04-23 20:09:46 -0300

    added new styles for mobile

--- sections/transparency/containers/DoughnutGraphic.js
+++ sections/transparency/containers/DoughnutGraphic.js
@@ -17,8 +17,20 @@ class DoughnutGraphic extends Component{
       <style jsx>{`
     .graphic-section {
       display: flex;
+      justify-content: space-around;
+      overflow-x: hidden;
+      height: auto;
+      
+    }
+    @media (max-width: 700px) {
+      .graphic-section {
+        display: flex;
+      flex-flow: column wrap;
       justify-content: center;
       overflow-x: hidden;
+
+      }
+      
     }
 
   `}</style>
