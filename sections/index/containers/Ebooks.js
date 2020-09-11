ec17aaa0d5e420048b6ba004bae8629fd22fc5ff
franz <franzisca@disroot.org>
2020-09-11 15:10:46 -0300

    Nueva publicación/ebook: desinformación

--- sections/index/containers/Ebooks.js
+++ sections/index/containers/Ebooks.js
@@ -3,7 +3,7 @@ import { t } from '../../../polyglot-modules/polyglot.js'
 import EbookCard from '../components/EbookCard'
 //let Flickity;
 
-const data = [0, 1, 2, 3]
+const data = [0, 1, 2, 3, 4]
 
 class Ebooks extends Component {
   constructor(props){
