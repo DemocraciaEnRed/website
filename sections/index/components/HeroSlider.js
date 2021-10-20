63d559aa48f0e6353fad8fcc1703b7fcdfa09413
lieta96 <julieta.garcia96@gmai.com>
2021-10-20 12:05:11 -0300

    nuevo banner me representa

--- sections/index/components/HeroSlider.js
+++ sections/index/components/HeroSlider.js
@@ -44,13 +44,18 @@ class SimpleSlider extends Component {
         />
     </Head>
     <Slider {...settings} className="hero-container">
-      <div className="slide se-voz">
-        <img src="../static/assets/banner-se-voz.jpg" className="is-hidden-tablet" alt=""/>
-        <img src="../static/assets/banner-se-voz-mobile.jpg" className="is-hidden-mobile" alt=""/>
-        <a className="button" href="https://preguntarparaacordar.typeform.com/to/VTI9VF52" target="_blank">SUBÍ TU MEME</a>
-      </div>
       <div>
       <Header  modal={this.state.modal} content={this.state.content} hideModal={this.hideModal} />
+      </div>
+      <div className="slide se-voz">
+        <a href="https://merepresenta.info/" target="_blank">
+          <img src="../static/assets/merre-desktop.png" className="is-hidden-tablet" alt="Banner de Me representa"/>
+        </a>
+        <a href="https://merepresenta.info/" target="_blank">
+          <img src="../static/assets/merre-mobile.png" className="is-hidden-desktop" alt="Banner de Me representa"/>
+        </a>
+        
+        
       </div>
     </Slider>
     <style jsx>{`
@@ -67,44 +72,22 @@ class SimpleSlider extends Component {
           overflow:hidden;
           position:relative
       }
-      .se-voz .button{
-        position:absolute;
-        bottom:17%;
-        right:40%
-        background: #000;
-        color: #fff794
-        padding:5px;
-        font-size:2rem
-      }
-        
       .se-voz img{
-        height: 70vh;
-        width: 100vw; 
-        object-fit: cover;
-
+        width: 100vw;
+        object-position: top;
+        object-fit: contain;
+        margin-top:30px
       }
-      @media only screen and (min-width:769px){
-        .is-hidden-mobile{
-          display:none
+      .is-hidden-desktop{
+        display: none
+      }
+    
+      @media only screen and (max-width:480px){
+        .is-hidden-desktop{
+          display: block;
         }
-      }
-      @media only screen and (max-width:768px){
         .is-hidden-tablet{
           display:none
-        }
-        .se-voz img{
-        height:auto;
-        }
-        .se-voz .button{
-          font-size:1rem;
-          right:21%
-          bottom.20%
-        }
-      }
-      @media only screen and (max-width:480px){
-        .se-voz .button{
-          bottom:22%;
-          right:28%
         }
       }
     `}</style>
